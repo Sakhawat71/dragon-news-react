0a3fb8015a0a912013a2c6cb1eb8029172972f44
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-24 14:10:57 +0600

    header complete

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,16 +1,19 @@
 import moment from "moment/moment";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
     return (
         <div className="flex flex-col justify-center items-center mt-5">
-            <h1 className="font-oldEng text-6xl text-dark2 mb-3">The Dragon News</h1>
+            <figure>
+                <img src={logo} alt="" />
+            </figure>
             <p className="text-lg text-dark1">Journalism Without Fear or Favour</p>
 
             <p className="text-xl font-medium text-dark1">
-                <span
-                    className="text-dark3">
-                    {moment().format('dddd')}
-                </span> {moment().format('MMMM D YYYY')}
+                {moment().format('dddd, ')}
+                <span className="text-dark3">
+                    {moment().format('MMMM D YYYY')}
+                </span>
             </p>
         </div>
     );
---
+++ src/pages/Home/BreakingNews.jsx
@@ -0,0 +1,10 @@
+
+const BreakingNews = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BreakingNews;
