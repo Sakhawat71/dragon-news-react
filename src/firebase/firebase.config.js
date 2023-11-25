0cd91237bb536eec94a920c3d841ffc77d30a27b
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-26 02:19:37 +0600

    firebase and env.local

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDiSO22Gy5FOvTL7d5_6e6NAAWRIOwZH4k",
-    authDomain: "dragon-news-react-86b1e.firebaseapp.com",
-    projectId: "dragon-news-react-86b1e",
-    storageBucket: "dragon-news-react-86b1e.appspot.com",
-    messagingSenderId: "102050143343",
-    appId: "1:102050143343:web:48fd3a4ca9e1efe5f7419a"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 
